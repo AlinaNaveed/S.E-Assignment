c2e52edeffc013d2bd0f5a0450e56333446bddb8
AlinaNaveed <126234044+AlinaNaveed@users.noreply.github.com>
2023-05-17 22:36:02 +0500

    Update Sample2Class.java
    updated line of code

--- SampleProject/src/Com/Sample/TestScripts/Sample2Class.java
+++ SampleProject/src/Com/Sample/TestScripts/Sample2Class.java
@@ -4,6 +4,7 @@ public class Sample2Class {
     public void someMethod() {
         System.out.println("Hello from the second method!");
         System.out.println("Hello from Paras roll number 86");
+        system.out.println("Heelo from Alina roll number 13"):
         
     }
 }
