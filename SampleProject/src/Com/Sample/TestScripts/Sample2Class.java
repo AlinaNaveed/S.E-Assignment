7e84c4e60887422268eb870fd94b7f499e5b46d1
AlinaNaveed <126234044+AlinaNaveed@users.noreply.github.com>
2023-05-17 22:29:56 +0500

    Update Sample2Class.java
    updates a line of code!

--- SampleProject/src/Com/Sample/TestScripts/Sample2Class.java
+++ SampleProject/src/Com/Sample/TestScripts/Sample2Class.java
@@ -4,5 +4,6 @@ public class Sample2Class {
     public void someMethod() {
         System.out.println("Hello from the second method!");
         System.out.println("Hello from Paras roll number 86");
+        
     }
 }
